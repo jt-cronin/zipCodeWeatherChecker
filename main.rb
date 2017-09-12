71c043da4580386af48b5ab17924fb732aca8ee5
John T Cronin <jt.cronin@outlook.com>
2017-09-12 13:32:54 -0500

    time to make test that can be run

--- main.rb
+++ main.rb
@@ -5,9 +5,16 @@ require 'json'
 
 
 
-def zipCodeCheck
-	
+def zipCodeCheck(a)
+	code = "#{a}"
+	until (code.length == 5) && (code.to_i > 9999)
+		puts "Your code must only have five numbers"
+		code = gets.chomp
+	end
+	return code
 end
+
+
 
 
 welcome = "What zip code would you like to look up?"
@@ -15,15 +22,17 @@ welcome = "What zip code would you like to look up?"
 puts welcome
 
 zipCode = gets.chomp
-
+zipCode = zipCodeCheck(zipCode)
 
 website = open("http://maps.googleapis.com/maps/api/geocode/json?components=postal_code:#{zipCode}")
 response_status = website.status
 response_body = website.read
 response_JSON = JSON.parse(response_body)
 
+
 puts response_status
-puts response_JSON['results'][0]
+binding.pry
+puts response_JSON['results'][0]['geometry']['location']
 
 
 
