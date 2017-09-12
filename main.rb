2a3fe0b6c963713a0b4ef765eb008a9328f5403b
John T Cronin <jt.cronin@outlook.com>
2017-09-12 11:18:37 -0500

    have basics, about to write tests

---
+++ main.rb
@@ -0,0 +1,18 @@
+require 'open-uri'
+require 'pry'
+require 'json'
+
+
+welcome = "What zip code would you like to look up?"
+
+
+puts welcome
+zipCode = gets.chomp
+
+website = open("http://maps.googleapis.com/maps/api/geocode/json?components=postal_code:#{zipCode}")
+response_status = website.status
+response_body = website.read
+response_JSON = JSON.parse(response_body)
+
+puts response_status
+puts response_JSON['results'][2]
